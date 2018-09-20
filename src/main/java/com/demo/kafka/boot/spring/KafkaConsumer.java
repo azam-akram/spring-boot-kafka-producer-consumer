f08461ce5e37934ac1c329ac7133a660226a0899
AzamAkram <akram.muhammadazam@gmail.com>
2018-09-20 22:33:31 +0200

    USe only spring boot kafka properties to run producer and consumer

--- src/main/java/com/demo/kafka/boot/spring/KafkaConsumer.java
+++ src/main/java/com/demo/kafka/boot/spring/KafkaConsumer.java
@@ -1,14 +1,17 @@
 package com.demo.kafka.boot.spring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
 public class KafkaConsumer {
     @KafkaListener(topics = "${application.topic.common-error}", groupId = "${spring.kafka.consumer.group-id}")
-    public void listen(String message) {
-        log.trace("Received Messasge: " + message);
+    public void onMessage(final ConsumerRecord<String, String> consumerRecord) throws IOException {
+        log.trace("Received Messasge: " + consumerRecord.value());
     }
 }
